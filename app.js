e5f0110b3338779c459512db3a7640c830c388a6
shivanijaiswar13 <shivanijaiswar01@gmail.com>
2026-04-06 17:02:03 +0530

    add some code

---
+++ app.js
@@ -0,0 +1,35 @@
+const BASE_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+const dropdowns =document.querySelectorAll(".dropdown select")
+const btn = document.querySelector("form button");
+
+
+for(let select of dropdowns){
+    for (currCode in countryList){
+    let newOption = document.createElement("option")
+    newOption.innerText = currCode;
+    newOption.value = currCode;
+    if(select.name === "from" && currCode === "USD"){
+        newOption.selected = "selected";
+    }else if(select.name === "to" && currCode === "INR"){
+        newOption.selected = "selected";
+    }
+    select.append(newOption);
+}
+select.addEventListener("change", (evt)=>{
+    updateFlag(evt.target);
+
+})
+}
+
+const updateFlag = (element)=>{
+    let currCode =element.value;
+    countryCode = countryList[currCode];
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`
+    let img = element.parentElement.querySelector("img");
+    img.src = newSrc;
+};
+
+btn.addEventListener("click",(evt)=>{
+    evt.preventDefault();
+    let amount = document.querySelector("form input");
+})
